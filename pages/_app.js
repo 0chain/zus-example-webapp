93ebb6ee222f337ae655d569ea7f41bd27287a2e
ranajahanzaib <iam@ranajahanzaib.com>
2023-06-01 17:09:04 +0500

    Fix 404 Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "../src/styles/globals.scss";
+import { App as MainApp } from "@/components/App/App";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MainApp>
+      <Component {...pageProps} />
+    </MainApp>
+  );
 }
---
+++ src/screens/details/index.tsx
@@ -0,0 +1,5 @@
+import WalletDetails from "./WalletDetails";
+import AllocationDetails from "./AllocationDetails";
+import NetworkDetails from "./NetworkDetails";
+
+export { WalletDetails, AllocationDetails, NetworkDetails }
