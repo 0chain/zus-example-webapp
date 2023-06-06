6cc609f32d82512fc4b70e66c928781dad8f9e67
Usama MK <usamamuhammad45678@gmail.com>
2023-06-06 06:53:24 +0500

    added faucet dialog (#64)
    * added faucet dialog
    
    * default amount 10

---
+++ src/components/dialog/faucet-token/FaucetTokenDialog.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+import { Dialog } from 'components/dialog'
+
+import { getFaucetToken } from '@zerochain/zus-sdk'
+
+import stl from './FaucetTokenDialog.module.scss'
+
+const FaucetTokenDialog = ({ close, setIsSuccess, setIsError }) => {
+  const [amount, setAmount] = useState(10)
+
+  const onFaucet = async () => {
+    try {
+      await getFaucetToken(amount)
+    } catch (error) {
+      console.log(error)
+      setIsError(true)
+      close()
+      return
+    }
+
+    setIsSuccess(true)
+    close()
+  }
+
+  return (
+    <Dialog theme="bolt" close={close}>
+      <Dialog.Header title="Faucet Tokens" close={close}></Dialog.Header>
+
+      <input
+        id="amount"
+        name="amount"
+        value={amount}
+        onChange={e => setAmount(e.target.value)}
+        placeholder="Amount"
+        className={stl.textInputBox}
+      />
+
+      <Dialog.Footer
+        actionButtonLabel="Faucet"
+        actionButtonOnClick={onFaucet}
+      ></Dialog.Footer>
+    </Dialog>
+  )
+}
+
+export default FaucetTokenDialog
--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,7 +1,9 @@
 import Dialog from './Dialog'
 import { SendTokenDialog } from './send-token'
 import { ReceiveTokenDialog } from './receive-token'
+import { FaucetTokenDialog } from './faucet-token'
 
 export { SendTokenDialog }
 export { ReceiveTokenDialog }
-export { Dialog }
+export { FaucetTokenDialog }
+export default Dialog
