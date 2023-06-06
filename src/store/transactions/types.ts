9e3eda2ae81fb859d873ebebac21e2b904876947
ranajahanzaib <iam@ranajahanzaib.com>
2023-06-06 08:03:21 +0500

    Declare Types

--- src/store/transactions/types.ts
+++ src/store/transactions/types.ts
@@ -1,8 +1,8 @@
 import { createActionTypes } from 'lib'
+import { Types } from 'store/api-utils'
 
 export const GET_LATEST_TXNS = 'GET_LATEST_TXNS'
-export const GET_LATEST_TXNS_REQUEST = 'GET_LATEST_TXNS_REQUEST'
-export const GET_LATEST_TXNS_SUCCESS = 'GET_LATEST_TXNS_SUCCESS'
-export const GET_LATEST_TXNS_ERROR = 'GET_LATEST_TXNS_ERROR'
 
-export default createActionTypes([GET_LATEST_TXNS])
+const types: Types = createActionTypes([GET_LATEST_TXNS])
+
+export default types
